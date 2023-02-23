431947fb299c5193e7205c6f60ac40bc620f3d2b
farshad-vgr <farshadvaghari@gmail.com>
2023-02-23 16:55:32 +0330

    :root added for colors

--- index.js
+++ index.js
@@ -5,60 +5,60 @@ const xhr = new XMLHttpRequest();
 xhr.open("GET", "https://picsum.photos/v2/list");
 
 xhr.onload = function () {
-  if (xhr.status == 200) {
-    const images = JSON.parse(xhr.responseText).splice(10, 5);
+	if (xhr.status == 200) {
+		const images = JSON.parse(xhr.responseText).splice(10, 5);
 
-    for (const image of images) {
-      const html = `
+		for (const image of images) {
+			const html = `
             <p>Author: <br> ${image.author}</p>
             <a href="${image.url}" target="_blank">visit page!</a>
           `;
 
-      const section = document.createElement("section");
-      section.classList.add("show");
-      section.innerHTML = html;
+			const section = document.createElement("section");
+			section.classList.add("show");
+			section.innerHTML = html;
 
-      const article = document.createElement("article");
-      article.classList.add("panel");
-      article.style.backgroundImage = `url('${image.download_url}')`;
+			const article = document.createElement("article");
+			article.classList.add("panel");
+			article.style.backgroundImage = `url('${image.download_url}')`;
 
-      article.append(section);
-      mainContainer.append(article);
+			article.append(section);
+			mainContainer.append(article);
 
-      article.addEventListener("click", () => {
-        const cards = [...document.querySelectorAll(".panel")];
-        const sections = [...document.querySelectorAll(".panel section")];
+			article.addEventListener("click", () => {
+				const cards = [...document.querySelectorAll(".panel")];
+				const sections = [...document.querySelectorAll(".panel section")];
 
-        function removeActiveClass() {
-          for (const card of cards) {
-            card.classList.remove("active");
-          }
-        }
+				function removeActiveClass() {
+					for (const card of cards) {
+						card.classList.remove("active");
+					}
+				}
 
-        function removeShowClass() {
-          for (const section of sections) {
-            section.classList.remove("show");
-          }
-        }
+				function removeShowClass() {
+					for (const section of sections) {
+						section.classList.remove("show");
+					}
+				}
 
-        function addShowClass() {
-          for (const section of sections) {
-            section.classList.add("show");
-          }
-        }
+				function addShowClass() {
+					for (const section of sections) {
+						section.classList.add("show");
+					}
+				}
 
-        if (article.classList.contains("active")) {
-          article.classList.remove("active");
-          addShowClass();
-        } else {
-          removeActiveClass();
-          article.classList.add("active");
-          removeShowClass();
-          article.querySelector("section").classList.add("show");
-        }
-      });
-    }
-  }
+				if (article.classList.contains("active")) {
+					article.classList.remove("active");
+					addShowClass();
+				} else {
+					removeActiveClass();
+					article.classList.add("active");
+					removeShowClass();
+					article.querySelector("section").classList.add("show");
+				}
+			});
+		}
+	}
 };
 
 xhr.send();
